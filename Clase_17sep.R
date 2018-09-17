7feb64e817e194290f368f5d2ad4ecc0aabb943b
SantiagodeMello <demello.santiago@gmail.com>
2018-09-17 17:15:11 -0300

    sep 17 v2

--- Clase_17sep.R
+++ Clase_17sep.R
@@ -1,3 +1,19 @@
 library("tidyverse")
 
-mtcars %>% group_by(cyl)
+a=mpg
+
+# Eje a)
+mpg %>% group_by(manufacturer="toyota") %>% group_by(model="camy") %>% select(manufacturer,model,year,cyl,cty)
+
+
+# Eje b)
+mpg %>% group_by(manufacturer) %>% summarise(mn = mean(cty)) %>% filter(mn=min | mn=max)
+
+
+# Eje c)  
+mpg %>% group_by(manufacturer) %>% summarise(rend.mn = mean(cty)) %>% filter()
+
+
+# Eje d)
+
+# Eje e)
